5659aaba87714769ca33318353283a3e187f6d42
HP <20060339@stu.omu.edu.tr>
2023-03-04 23:48:57 +0300

    edit

---
+++ KuvvetBulanProgram.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class KuvvetBulanProgram {
+  public static void main(String[] args) {
+
+    int sayi;
+
+    Scanner input = new Scanner(System.in);
+    System.out.print("Lütfen bir sayı giriniz : ");
+    sayi = input.nextInt();
+    
+  }
+  
+}
--- çiftVe4unToplami.java
+++ çiftVe4unToplami.java
@@ -3,19 +3,6 @@ import java.util.Scanner;
 public class çiftVe4unToplami {
     public static void main(String[] args) {
 
-      int sayi;
-
-      while(sayi<0){
-
-        Scanner input = new Scanner(System.in);
-        System.out.print("Lütfen bir sayı giriniz : ");
-        sayi = input.nextInt();
-
-        if(sayi%2 == 1){
-          System.out.println("devam");
-        }
-      }
-
 
     
   }
