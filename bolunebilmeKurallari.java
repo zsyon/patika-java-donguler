a68bd3cb8966f6c5320a7e8e5c2236cec3b8c156
HP <20060339@stu.omu.edu.tr>
2023-03-04 16:32:30 +0300

    edit

--- bolunebilmeKurallari.java
+++ bolunebilmeKurallari.java
@@ -9,14 +9,22 @@ public class bolunebilmeKurallari {
     System.out.println("Lütfen sayı giriniz : ");
     Scanner input = new Scanner(System.in);
     int sayi = input.nextInt();
+    int sayi3=0;
 
     for(int i=0 ; i<sayi ; i++){
 
-      if(i%2==0){
-        System.out.println(i);
-                
+      if(i%3==0){
+        sayi3 += i;      
       }
+      
+      if(i%4==0){
+        System.out.println(i);
+      }
+
+
     }
+
+    //System.out.println("3'ün Katlarının toplamı : " + sayi3);
     
   }
 
