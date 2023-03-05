05ecb5f5ca12ba5364fd00aac6467afee286fc3f
HP <20060339@stu.omu.edu.tr>
2023-03-05 22:48:31 +0300

    add loop

--- mukemmelSayiBulanProgram.java
+++ mukemmelSayiBulanProgram.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * mukemmelSayiBulanProgram
+ */
 public class mukemmelSayiBulanProgram {
   public static void main(String[] args) {
     
+    System.out.println("Lütfen sayı giriniz : ");
+    Scanner input = new Scanner(System.in);
+    int sayi = input.nextInt();
+    int sayi3=0;
+    int sayi4=0;
+
+    for(int i=0 ; i<sayi ; i++){
+
+      System.out.println(sayi);
+
+
+    }
   }
-  
 }
