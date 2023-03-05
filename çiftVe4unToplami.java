ade8dd5d3573739667e00c689cb31ef3c622bd7c
HP <20060339@stu.omu.edu.tr>
2023-03-05 17:14:25 +0300

    edit

--- çiftVe4unToplami.java
+++ çiftVe4unToplami.java
@@ -4,11 +4,17 @@ public class çiftVe4unToplami {
     public static void main(String[] args) {
 
       int sayi;
+      int cift=0;
       Scanner input = new Scanner(System.in);
 
       do {
         System.out.print("Lütfen bir sayı giriniz : ");
         sayi = input.nextInt();
+
+        if(sayi%4 == 0){
+          cift += sayi;
+          System.out.println("Toplam "+ cift);
+        }
         
       } while (sayi>0);
 
