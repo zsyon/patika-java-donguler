3292ea1772b09a683d2bfb84422efd8d65a6a387
HP <20060339@stu.omu.edu.tr>
2023-03-05 16:35:48 +0300

    add loop

--- çiftVe4unToplami.java
+++ çiftVe4unToplami.java
@@ -4,12 +4,15 @@ public class çiftVe4unToplami {
     public static void main(String[] args) {
 
       int sayi;
+      Scanner input = new Scanner(System.in);
 
-      while(true){
-        Scanner input = new Scanner(System.in);
+      do {
+        System.out.print("Lütfen bir sayı giriniz : ");
         sayi = input.nextInt();
+        
+      } while (sayi>0);
 
-      }
+      
 
 
 
